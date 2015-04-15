b4da0bb6bd8691c209ca6dacae04b2fee4344c6c
frant-git <frant089@gmail.com>
2015-04-15 17:13:27 -0300

    struct, create and destroy

--- lab2/spellchecker/loosely-coupled/src/dictionary.c
+++ lab2/spellchecker/loosely-coupled/src/dictionary.c
@@ -1,7 +1,11 @@
 #include "dictionary.h"
 
 struct Dictionary {
-    /*estructura*/
+    /* Completar aqui */
+    char** dict_main;
+    char** dict_ignored;
+    int main_size
+    int ignored_size;
 };
 
 void dict_load(*char filename);
@@ -11,7 +15,47 @@ void dict_save(char* fname);
 void dict_add(char* word);
 
 dict_contains();
-dict_new();
-dict_destroy();
+
+dict_s dict_new(void){
+	
+	dict_s new_dict = NULL;
+	new_dict = calloc(1,sizeof(struct Dictionary));
+
+	new_dict->dict_main = NULL;
+	new_dict->dict_ignored = NULL;
+	new_dict->main_size = 10;
+	new_dict->ignored_size = 0;
+
+	return new_dict;
+
+}
+
+dict_s dict_destroy(dict_s d){
+
+	int i,j = 0;
+	/* liberamos todo el dict_main */
+	for(i;i<d->main_size;i++){
+		free(d->dict_main[i])
+		d->dict_main[i] = NULL;
+		i++;
+	}
+	/* liberamos todo el dict_ignored */
+	for(j;j<d->ignored_size;j++){
+		free(d->dict_ignored[j])
+		d->dict_ignored[i] = NULL;
+		j++;
+	}
+	
+	free(dict_main);
+	dict_main = NULL;
+
+	free(dict_ignored);
+	dict_ignored = NULL;
+	
+	free(d);
+	d = NULL;
+
+	return d;
+}
 
 ignored_add();
