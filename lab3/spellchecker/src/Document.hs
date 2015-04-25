60dd763901eb5ddfa27f9ccef3bdbaa296470795
frant-git <frant089@gmail.com>
2015-04-25 12:11:49 -0300

    add document.hs

--- lab3/spellchecker/src/Document.hs
+++ lab3/spellchecker/src/Document.hs
@@ -6,7 +6,7 @@ module Document where
 
 type Word = String
 
-data Document = 
+data Document = Doc Handle Handle
 
 -- Abre los archivos especificados por los paths
 -- pasados como argumentos. El primer path repre-
@@ -14,9 +14,18 @@ data Document =
 -- segundo path representa el archivo de salida
 -- donde se guarda el documento ya procesado.
 doc_open :: FilePath -> FilePath -> IO Document
+doc_open fp1 fp2 = 
+    do file_in <- openFile fp1 ReadMode
+       file_out <- openFile fp2 WriteMode
+       return (Doc file_in file_out)
 
 -- Cierra los archivos especificados
 doc_close :: Document -> IO ()
+doc_close (Doc f1 f2) = 
+    do hclose f1
+       hclose f2
+       return ()
+
 
 -- Obtiene una palabra del documento especificado,
 -- copiando todos los caracteres no alfabeticos
@@ -24,6 +33,14 @@ doc_close :: Document -> IO ()
 -- Cuando alcanza el final del documento, lo señaliza
 -- con una excepcion.
 doc_get_word :: Document -> IO Word
+usar hgetchar no hace falta trabajar con la excepcion porq de eso se ocupa hgetchar
+cuando llega a eof solo levanta la excepcion
 
 -- Escribe una palabra en el documento de salida.
 doc_put_word :: Word -> Document -> IO ()
+doc_put_word word f = 
+    do writeFile f word
+    // hputs
+    // hputstring busco
+    
+       return ()
