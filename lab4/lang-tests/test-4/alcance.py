43bd1a55059a52af2f270949cb46ad5de4761601
frant-git <frant089@gmail.com>
2015-05-06 21:06:34 -0300

    terminado py and rb ej4

--- lab4/lang-tests/test-4/alcance.py
+++ lab4/lang-tests/test-4/alcance.py
@@ -1,5 +1,5 @@
 """
-python tiene alcance estatico ??
+python tiene alcance ESTATICO ??
 """
 
 global_var = 1
@@ -7,13 +7,14 @@ global_var = 1
 def function_a(z):
 	print "function_a, z-> ", z
 	print "function_a, global_var-> ", global_var
-	print "return: ", global_var+z
+	print "return: ", (global_var+z)
 	return global_var+z
 
 def function_b(y):
 	print "function_b, y-> ", y
 	global_var = y+1
 	print "function_b, global_var-> ", global_var
+	print "function_b, return: ", (global_var*y)
 	return function_a(global_var*y)
 
 function_b(3)
---
+++ lab4/lang-tests/test-4/alcance.rb
@@ -0,0 +1,33 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+=begin
+Ruby es de alcance DINAMICO
+=end
+
+$global_var = 1
+
+def function_a(z)
+	print "function_a, z --> ", z
+	print "\n"
+	print "function_a, global_var --> ", $global_var
+	print "\n"
+	print "resultado de function_a = ", ($global_var + z)
+	print "\n"
+	return $global_var + z
+
+end
+
+def function_b(y)
+	print "function_b, y --> ", y
+	print "\n"
+	$global_var = y+1
+	print "function_b, global_var --> ", $global_var
+	print "\n"
+	print "function_b, return: ", ($global_var*y)
+	print "\n"
+	return function_a($global_var*y)
+
+end
+
+function_b(3)
