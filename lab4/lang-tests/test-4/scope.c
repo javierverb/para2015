abf23f2ac35276884bfdb3a10822a40248076359
frant-git <frant089@gmail.com>
2015-05-14 19:23:41 -0300

    add informe test4

--- lab4/lang-tests/test-4/scope.c
+++ lab4/lang-tests/test-4/scope.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 
-
 int global = 1;
 
 int function_a(int z){
-    printf("z en function_a vale: %d\n",z);
-    printf("global en function_a vale: %d\n",global);
-    printf("como global vale 4 entonces tomo el valor del bloque en function_b\n");
+    printf("en function_a 'z' vale: %d\n",z);
+    printf("en function_a 'global' vale: %d\n",global);
+    printf("como 'global' vale 4 entonces tomo el valor del bloque en function_b\n");
     printf("entonces C tiene un ALCANCE ESTATICO\n");
     return z + global;
 }
 
 int function_b(int y){
-    printf("\ny en function_b vale: %d\n",y);
+    printf("\nen function_b 'y' vale: %d\n",y);
     int global = y+1;
-    printf("global es local para function_b y vale: %d\n\n",global);
+    printf("'global' es local para function_b y vale: %d\n\n",global);
     return (function_a(global*y));
 }
-
 
 int main(void){
     int res = 0;
--- lab4/lang-tests/test-4/scope.java
+++ lab4/lang-tests/test-4/scope.java
@@ -5,9 +5,9 @@ class Global {
 class Alcance {
     
     public static int fun_a(int z){
-        System.out.println("esto es z en fun_a");
+        System.out.println("esto es 'z' en fun_a");
         System.out.println(z);
-        System.out.println("esto es global_var en fun_a");
+        System.out.println("esto es 'global_var' en fun_a");
         System.out.println(Global.global_var);
         return (z + Global.global_var);
     }
@@ -15,19 +15,19 @@ class Alcance {
     public static int fun_b(int y){
         /*int global_var = 0;*/
 
-        System.out.println("esto es y en fun_b");
+        System.out.println("esto es 'y' en fun_b");
         System.out.println(y);
-        System.out.println("esto es global_var en fun_b");
+        System.out.println("esto es 'global_var' en fun_b");
         System.out.println(Global.global_var);
 
         Global.global_var = y + 1;
-        System.out.println("ahora global_var = y+1 (sigo en fun_b)");
+        System.out.println("ahora 'global_var' = y+1 (sigo en fun_b)");
         System.out.println(Global.global_var);
         return fun_a(Global.global_var * y);
     }
 
     public static void main(String[] args){
-        System.out.println("hola a todos esto es main");
+        System.out.println("main");
         
         int result;
         result = fun_b(3);
