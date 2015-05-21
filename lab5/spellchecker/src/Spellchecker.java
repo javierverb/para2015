9dc01f74d8ae4328c717fe30e4116d4b3b14d128
frant-git <frant089@gmail.com>
2015-05-21 17:18:27 -0300

    add methods to Spellchecker.java

--- lab5/spellchecker/src/Spellchecker.java
+++ lab5/spellchecker/src/Spellchecker.java
@@ -1,9 +1,22 @@
 
 public class Spellchecker {
 
+	// CONSTRUCTOR
+		public Spellchecker(){}
+		
+		
+	// METHODS
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public static Word consultUser(Word w, Dictionary dic_in, Dictionary dic_out){
+		
+	}
+	
+	public static void processDocument(String s1, String s2, Dictionary dic_in, Dictionary dic_out){
+		
 	}
 
 }
