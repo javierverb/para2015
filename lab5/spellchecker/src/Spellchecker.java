f78eafe21e8405903d19f568446a90eb7ca6c540
frant-git <frant089@gmail.com>
2015-05-26 13:31:18 -0300

    add consultUser and proccesDocument

--- lab5/spellchecker/src/Spellchecker.java
+++ lab5/spellchecker/src/Spellchecker.java
@@ -11,70 +11,97 @@ import word.Word;
  */
 public class Spellchecker {
 
-    // CONSTRUCTOR
-    public Spellchecker(){}
-        
-        
-    // METHODS
-    public static void main(String[] args) {
-        
-        String dict;
-        
-        if(args.length < 2){
-            System.out.println("spellchecker.c: nro de argumentos erroneo.");
-            System.out.println("Deben ser <documento> [<diccionario>].");
-            // return (1);
-        }
-        
-        // si se especifico un diccionario lo usamos,
-        // caso contrario usamos el diccionario por defecto
-        dict = (args.length >=3) ? args[2] : "dict.txt";
+	// CONSTRUCTOR
+	public Spellchecker(){}
+		
+		
+	// METHODS
+	public static void main(String[] args) {
+		
+		String dict;
+		
+		if(args.length < 2){
+			System.out.println("spellchecker.c: nro de argumentos erroneo.");
+			System.out.println("Deben ser <documento> [<diccionario>].");
+			// return (1);
+		}
+		
+		// si se especifico un diccionario lo usamos,
+		// caso contrario usamos el diccionario por defecto
+		dict = (args.length >=3) ? args[2] : "dict.txt";
+		
+		// create dictionaries
+		FileDictionary dict_main = new FileDictionary();
+		MemDictionary dict_ignored = new MemDictionary();
+		// load dict_main
+		dict_main.load(dict);
+		// procces document
+		processDocument("doc_in.txt", "doc_out.txt", dict_main, dict_ignored);
+		// save dict_main
+		dict_main.save();
+		// destroy dictionaries
+		dict_main.clear();
+		dict_ignored.clear();
 
-        // FALTA COMPLETAR
+	}
+	
+	public static Word consultUser(Word w, Dictionary dic_in, Dictionary dic_out) {
+		
+		
+		String s;
+		String rep;
+		
+		do{
+			System.out.println("Palabra no reconocida: " + w);
+			System.out.println("Aceptar (a) - Ignorar (i) - Reemplazar (r): ");
+			Scanner reader = new Scanner(System.in);
+			s = reader.next();
+			reader.close();
+		}while ((s != "r") && (s != "a") && (s != "i"));
+		
+		if (s == "a") {
+			// hacer
+			System.out.println("la palabra: " + w);
+			System.out.println("fue AGREGADA al diccionario");
+		}
 
-    }
-    
-    public static Word consultUser(Word w, Dictionary dic_in, Dictionary dic_out) {
-        
-        
-        String s;
-        String rep;
-        
-        do{
-            System.out.println("Palabra no reconocida: " + w);
-            System.out.println("Aceptar (a) - Ignorar (i) - Reemplazar (r): ");
-            Scanner reader = new Scanner(System.in);
-            s = reader.next();
-            reader.close();
-        }while ((s != "r") && (s != "a") && (s != "i"));
-        
-        if (s == "a") {
-            // hacer
-            System.out.println("la palabra: " + w);
-            System.out.println("fue AGREGADA al diccionario");
-        }
-
-        if (s == "i") {
-            // hacer
-            System.out.println("la palabra: " + w);
-            System.out.println("fue IGNORADA al diccionario");
-        }
-        
-        if (s == "r") {
-            // remlazar
-            System.out.println("Remplazar por: ");
-            Scanner replace = new Scanner(System.in);
-            rep = replace.next();
-            replace.close();
-            w.setWord(rep);
-        }
-        
-        return w;
-            
-    }
-    
-    public static void processDocument(String s1, String s2, Dictionary dic_in, Dictionary dic_out){
-        // TODO: complete this!!
-    }
+		if (s == "i") {
+			// hacer
+			System.out.println("la palabra: " + w);
+			System.out.println("fue IGNORADA al diccionario");
+		}
+		
+		if (s == "r") {
+			// remlazar
+			System.out.println("Remplazar por: ");
+			Scanner replace = new Scanner(System.in);
+			rep = replace.next();
+			replace.close();
+			w.setWord(rep);
+		}
+		
+		return w;
+			
+	}
+	
+	public static void processDocument(String s1, String s2, Dictionary dic_in, Dictionary dic_out){
+		// TODO: complete this!!
+		Word current_word;
+		Document doc = new Document(doc_in, doc_out);
+		
+		try{
+			Word wd;
+			while(true){
+				current_word = doc.getWord();
+				if(!(dic_added_word.contains(current_word) || dic_ignored_word.contains(current_word))){
+					wd = consultUser(current_word, dic_added_word, dic_ignored_word);
+				}
+				doc.putWord(wd);	
+			}
+		}
+		catch (IOError){
+			doc.close();
+		}
+	}
 
 }
