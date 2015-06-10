abfac68493224a922f5fd32e24c2d4cd78257690
javierverb <javier2mil8@gmail.com>
2015-06-10 14:49:47 -0300

    update splk

--- lab6/spellchecker/spellchecker.sh
+++ lab6/spellchecker/spellchecker.sh
@@ -1,45 +1,106 @@
 #!/bin/bash
-# data file
-INPUT="test.txt"
+    
+# Algunas fuentes:
+# http://www.linuxjournal.com/content/return-values-bash-functions
+# http://stackoverflow.com/questions/3236871/how-to-return-a-string-value-from-a-bash-function
+# http://stackoverflow.com/questions/1878852/how-to-enter-data-from-keyboard-in-shell-programming
+
+shopt -s expand_aliases # EXAMPLES:
+alias is_contains='grep -ci' # return 1 if found, else 0
+alias dict_new='touch' # touch $name_dict
+alias dict_destroy='rm' # rm $dict_target
+alias put_word='echo' # put_word '$word' >> '$destiny_file'
+alias dict_add='echo' # dict_add '$word' >> '$destiny_file'
+
+GLOBAL_WORD_ACUMULATOR=""
+
+# dict added word
+GLOBAL_DICTIONARY=""
+# dict ignored word
+GLOBAL_IGNORED_DICTIONARY=".dict_ignored.txt"
+
+function get_len_file {
+    du -b empty.txt | grep -oh [0-9]*
+    echo $?
+}
+
+
 function get_word {
-    # while loop
-    local acumulator=""
+    
+    local document=$1
     while IFS= read -r -n1 character
     do
         # check if is valid
-        if ! [[ "$char" =~ [^a-zA-Z0-9\ ] ]]; then
-            acumulator+=$character
+        if ! [[ "$character" =~ [^a-zA-Z0-9\ ] ]]; then
+            GLOBAL_WORD_ACUMULATOR+=$character
+        else
+            break;
         fi
-    # display one character at a time
-    done < "$INPUT"
-    echo $acumulator
+        echo $GLOBAL_WORD_ACUMULATOR
+    
+    done < "$document"
+    GLOBAL_WORD_ACUMULATOR="EOF"
 }
 
-function is_contains {
-    local is_kwnow=`grep $1 $2`
-    return is_kwnow 
+function consult_user {
+    local word=$1
+    local document=$2
+    echo "Palabra no reconocida: $word"
+    echo "Agregar al diccionario (a) - 
+          Ignorar (i) - 
+          Reemplazar (r): "
+    # get from stdin a option
+    read option_selected
+    
+    case "$option_selected" in
+        a)
+            dict_add $word >> $GLOBAL_DICTIONARY
+            ;;
+        i)
+            dict_add $word >> $GLOBAL_IGNORED_DICTIONARY
+            ;;
+        r)
+            echo "Por favor, Ingrese la nueva palabra
+                  con la que desea reemplazar $word"
+            read new_word
+            put_word $new_word >> document
+    esac
 }
-
-result=is_contains hola test.txt
 
 function main {
     
     # Verificamos el nro de argumentos.
-    if (("$#" < 1)); then
-        echo "spellchecker.c: nro de argumentos erroneo. Deben ser <documento> [<diccionario>].\n"
+    if (($# < 1)); then
+        echo "spellchecker.c: nro de argumentos erroneo. 
+              Deben ser <documento> [<diccionario>].\n"
         exit
     fi
 
-    # si se especifico un diccionario lo usamos,  
+    # si se especifico un diccionario lo usamos,
+    if (("$#" >= 2)); then
+        GLOBAL_DICTIONARY=$2
     # caso contrario usamos el diccionario por defecto
-    #if (("$#" >= 2)); then
-    #    dict = $2
-    #else
-    #    dict = "dict.txt"
-    #fi
+    else
+        dict_new "default_dict.txt" && dict_destroy "default_dict.txt"
+        GLOBAL_DICTIONARY="default_dict.txt"
+    fi
 
-
+    # process_document
+    document=$1
+    while :
+    do
+        get_word $document
+        if [[ $GLOBAL_WORD_ACUMULATOR="no" ]]; then
+            echo "$GLOBAL_WORD_ACUMULATOR" # (debería mostrar exit) 
+            break;
+        else
+            consult_user $GLOBAL_WORD_ACUMULATOR $document
+            # favor reinicializar la variable acumador
+        fi
+    done
 }
 
-
-main doc.tx
+# run script
+document=$1
+dictionary=$2
+main $document $dictionary
