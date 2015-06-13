20edc3d08dca7e1b60fa8d1968911deb09e00a53
javierverb <javier2mil8@gmail.com>
2015-06-12 23:35:37 -0300

    update spellchecker_magic_magic_magic

---
+++ lab6/spellchecker/clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm document.output
+rm default_dict.txt
+rm .dict_ignored.txt
--- lab6/spellchecker/spellchecker.sh
+++ lab6/spellchecker/spellchecker.sh
@@ -6,77 +6,93 @@
 # http://stackoverflow.com/questions/1878852/how-to-enter-data-from-keyboard-in-shell-programming
 
 shopt -s expand_aliases # EXAMPLES:
-alias is_known='grep -ci' # return 1 if found, else 0, is_contains hola $file
+# is_known "some_word" $file_to_found
+alias is_known='grep -ci' # return 1 if found, else 0
+alias put_word='echo -n' # put_word '$word' >> '$destiny_file'
 alias dict_new='touch' # touch $name_dict
-alias dict_destroy='rm' # rm $dict_target
-alias put_word='echo' # put_word '$word' >> '$destiny_file'
 alias dict_add='echo' # dict_add '$word' >> '$destiny_file'
-
+alias dict_destroy='rm' # rm $dict_target
 
 GLOBAL_WORD_ACUMULATOR=""
-GLOBAL_FILE_OUTPUT="file_output.txt"
+GLOBAL_FILE_OUTPUT="document.output"
+GLOBAL_ARRAY_WORDS=()
 
 # dict added word
 GLOBAL_DICTIONARY=""
 # dict ignored word
 GLOBAL_IGNORED_DICTIONARY=".dict_ignored.txt"
 
-function get_len_file {
-    du -b empty.txt | grep -oh [0-9]*
-    echo $?
-}
-
+GLOBAL_WORD_TO_PUT=""
 
 function get_word {
     
     local document=$1
-    while IFS= read -r -n1 character
+    local valid_word=0
+    local invalid_character=1
+
+    IFS=''
+    while read -r -n1 character
     do
         # check if is valid
-        if ! [[ "$character" =~ [^a-zA-Z0-9\ ] ]]; then
+        if ! [[ "$character" =~ [^a-zA-Z0-9] ]]; then
             GLOBAL_WORD_ACUMULATOR+=$character
         else
-            break;
+            GLOBAL_ARRAY_WORDS[$valid_word]=$GLOBAL_WORD_ACUMULATOR
+            GLOBAL_ARRAY_WORDS[$invalid_character]=$character
+            GLOBAL_WORD_ACUMULATOR=""
         fi
-        echo $GLOBAL_WORD_ACUMULATOR
-    
+    let valid_word+=2
+    let invalid_character+=2
     done < "$document"
-    GLOBAL_WORD_ACUMULATOR="EOF"
 }
-
 function consult_user {
+    # Ejecuta Agregar/Ignorar/Reemplazar
+    # Según la opción seleccionada por el usuario
+    
     local word=$1
-    local document_output=$2
+    
     echo "Palabra no reconocida: $word"
     echo "Agregar al diccionario (a) - 
           Ignorar (i) - 
           Reemplazar (r): "
     # get from stdin a option
-    read -n1 option_selected
-    
-    case "$option_selected" in
-        a)
-            dict_add $word >> $GLOBAL_DICTIONARY
-            ;;
-        i)
-            dict_add $word >> $GLOBAL_IGNORED_DICTIONARY
-            ;;
-        r)
-            echo "Por favor, Ingrese la nueva palabra
-                  con la que desea reemplazar $word"
-            read new_word
-            word=new_word
-            ;;
-        *)
-            echo "Opcion invalida"
-            consult_user $word $document_output
-            ;;
-    esac
-    put_word $word >> document_output
+
+    read option_selected
+    while true; do
+        case "$option_selected" in
+            a)
+                dict_add $word >> $GLOBAL_DICTIONARY
+                break
+                ;;
+            i)
+                dict_add $word >> $GLOBAL_IGNORED_DICTIONARY
+                break
+                ;;
+            r)
+                echo "Ingrese la nueva palabra con la que desea reemplazar '$word'"
+                read word_to_replace
+                word=$word_to_replace
+                break
+                ;;
+            *)
+                echo "Opción inválida"
+                consult_user $word
+                break
+                ;;
+        esac
+    done
+    # 'return'
+    GLOBAL_WORD_TO_PUT=$word
+}
+
+function clean_old_output {
+    touch $GLOBAL_FILE_OUTPUT
+    rm $GLOBAL_FILE_OUTPUT
 }
 
 function main {
     
+    clean_old_output
     # Verificamos el nro de argumentos.
     if (($# < 1)); then
         echo "spellchecker.c: nro de argumentos erroneo. 
@@ -85,45 +101,44 @@ function main {
     fi
 
     # si se especifico un diccionario lo usamos,
-    if (("$#" >= 2)); then
+    if (($# >= 2)); then
         GLOBAL_DICTIONARY=$2
     # caso contrario usamos el diccionario por defecto
     else
+        echo "Diccionario no provisto. Usando diccionario por defecto"
         dict_new "default_dict.txt" && dict_destroy "default_dict.txt"
         GLOBAL_DICTIONARY="default_dict.txt"
     fi
+
+    dict_new $GLOBAL_DICTIONARY
     dict_new $GLOBAL_IGNORED_DICTIONARY
 
     # process_document
     document=$1
-    index=0
-    
-    while IFS= read -r -n1 character
-    do
-        # check if is valid
-        if ! [[ "$character" =~ [^a-zA-Z0-9\ ] ]]; then
-            GLOBAL_WORD_ACUMULATOR+=$character
-        else
-            if (index==0); then
-                put_word $character >> GLOBAL_FILE_OUTPUT
-            else
-                let known=$(is_known $GLOBAL_WORD_ACUMULATOR $GLOBAL_DICTIONARY) +
-                          $(is_know $GLOBAL_WORD_ACUMULATOR $GLOBAL_IGNORED_DICTIONARY)
-                if (know==0); then
-                    consult_user $GLOBAL_WORD_ACUMULATOR $GLOBAL_DICTIONARY
-                else
-                    put_word $GLOBAL_WORD_ACUMULATOR $GLOBAL_DICTIONARY
-                fi
-                GLOBAL_WORD_ACUMULATOR=""
-                index=0
+
+    get_word $document
+    for word in ${GLOBAL_ARRAY_WORDS[*]}; do
+        echo "WORD:[$word]"
+        GLOBAL_WORD_TO_PUT=$word
+
+        already_in_dict_added=$(is_known $word $GLOBAL_DICTIONARY)
+        already_in_dict_ignored=$(is_known $word $GLOBAL_IGNORED_DICTIONARY) 
+        
+        if ! [[ "$word" =~ [^a-zA-Z0-9] ]]; then
+            # si no es conocida
+            let is_known=already_in_dict_added+already_in_dict_ignored
+            if [[ $is_known -eq 0 ]]; then
+                consult_user $word
             fi
+            put_word $GLOBAL_WORD_TO_PUT >> $GLOBAL_FILE_OUTPUT
+        else
+            put_word $word >> $GLOBAL_FILE_OUTPUT
         fi
-        let index+=1
-    
-    done < "$document"
-    echo "El documento $document ha sido procesado. 
-    Resultados en $GLOBAL_FILE_OUTPUT"
-    dict_destroy GLOBAL_IGNORED_DICTIONARY
+    done
+
+    dict_destroy $GLOBAL_IGNORED_DICTIONARY
+    echo "El documento '$document' ha sido procesado. 
+    Resultados en '$GLOBAL_FILE_OUTPUT'"
 }
 
 # run script
